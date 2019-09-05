c1f1bc87667efbe4d6013397fae3c58d4a1137b4
Konstantin Tokarev <ktokarev@smartlabs.tv>
2019-09-05 16:15:11 +0300

    Use upstream repo for Otter

--- build-otter.sh
+++ build-otter.sh
@@ -1,5 +1,5 @@
 #!/bin/sh -ex
-git clone --depth 1 https://github.com/annulen/otter-browser.git -b getCreationTime
+git clone --depth 1 https://github.com/OtterBrowser/otter-browser.git
 mkdir otter-browser-build
 cmake -Botter-browser-build -Hotter-browser -DCMAKE_BUILD_TYPE=Release -DCMAKE_PREFIX_PATH=$QTDIR $CMAKE_ARGS -G "$CMAKE_GENERATOR"
 cmake --build otter-browser-build --config Release
