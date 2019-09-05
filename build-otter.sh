6f786a72a813f83ac5ef9b4b4c20b595c37a8ecb
Konstantin Tokarev <ktokarev@smartlabs.tv>
2019-09-05 16:40:27 +0300

    Introduce HOST_N_CORES instead of hardcoding 2

--- build-otter.sh
+++ build-otter.sh
@@ -2,4 +2,4 @@
 git clone --depth 1 https://github.com/OtterBrowser/otter-browser.git
 mkdir otter-browser-build
 cmake -Botter-browser-build -Hotter-browser -DCMAKE_BUILD_TYPE=Release -DCMAKE_PREFIX_PATH=$QTDIR $CMAKE_ARGS -G "$CMAKE_GENERATOR"
-cmake --build otter-browser-build --config Release
+cmake --build otter-browser-build --config Release -j $HOST_N_CORES
--- build-phantomjs.sh
+++ build-phantomjs.sh
@@ -5,7 +5,7 @@ UNAME=$(uname)
 git clone --depth 1 https://github.com/annulen/phantomjs.git
 cd phantomjs
 qmake
-$MAKE -j2
+$MAKE -j $HOST_N_CORES
 export QT_QPA_PLATFORM=minimal
 bin/phantomjs --version
 #test/run-tests.py --verbose
