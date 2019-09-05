c4b666f3dc15322d33b8f39a2b3994aa2c238667
Konstantin Tokarev <ktokarev@smartlabs.tv>
2019-09-06 01:35:29 +0300

    Build phantomjs with MinGW

--- build-phantomjs-vs.bat
+++ build-phantomjs-vs.bat
@@ -4,7 +4,6 @@ cd phantomjs
 cd && "C:\Program Files (x86)\Microsoft Visual Studio\2017\BuildTools\VC\Auxiliary\Build\vcvarsall.bat" %VS_ARCH% ^
   && qmake ^
   && jom ^
-  && dumpbin /dependents bin\phantomjs.exe ^
   && bin\phantomjs --version ^
   && REM python test\run-tests.py --verbose
 cd ..
