f19a3ef94073f1076db04a0007d1fbae4b915ff1
Konstantin Tokarev <ktokarev@smartlabs.tv>
2019-10-04 13:32:34 +0300

    Enable deploy on macOS

--- create-qa-archive.sh
+++ create-qa-archive.sh
@@ -8,9 +8,13 @@ find $QTDIR -name '*.prl' -exec rm {} \;
 find $QTDIR -name '*.lib' -exec rm {} \;
 find $QTDIR -name '*d.dll' -exec rm {} \;
 find $QTDIR -name '*.pdb' -exec rm {} \;
+find $QTDIR -name '*.dSYM' -exec rm -rf {} \; || true
+find $QTDIR -name '*_debug' -exec rm {} \;
+find $QTDIR -name '*_debug.dylib' -exec rm {} \;
 rm -rf $QTDIR/include
 rm -rf $QTDIR/doc
 rm -rf $QTDIR/mkspecs
 rm -rf $QTDIR/lib/cmake
 rm -rf $QTDIR/lib/pkgconfig
+rm -rf $QTDIR/debug
 7z a -m0=lzma -mx=9 -mfb=64 -md=32m -ms=on $ARCHIVE $QTDIR
