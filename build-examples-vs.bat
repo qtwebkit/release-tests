6dc447441290ed4c2b007b34829cce286dc2a13d
Konstantin Tokarev <annulen@yandex.ru>
2020-10-08 17:11:16 +0300

    Switch from VS2017 to VS2019

--- build-examples-vs.bat
+++ build-examples-vs.bat
@@ -1,6 +1,6 @@
 git clone --depth 1 -b no_example_installs https://github.com/annulen/qtwebkit-examples.git
 cd qtwebkit-examples\examples
-cd && "C:\Program Files (x86)\Microsoft Visual Studio\2017\BuildTools\VC\Auxiliary\Build\vcvarsall.bat" %VS_ARCH% ^
+cd && "C:\Program Files (x86)\Microsoft Visual Studio\2019\Community\VC\Auxiliary\Build\vcvarsall.bat" %VS_ARCH% ^
   && qmake ^
   && jom ^
   && jom install
--- build-phantomjs-vs.bat
+++ build-phantomjs-vs.bat
@@ -1,7 +1,7 @@
 git clone --depth 1 https://github.com/annulen/phantomjs.git
 set QT_QPA_PLATFORM=minimal
 cd phantomjs
-cd && "C:\Program Files (x86)\Microsoft Visual Studio\2017\BuildTools\VC\Auxiliary\Build\vcvarsall.bat" %VS_ARCH% ^
+cd && "C:\Program Files (x86)\Microsoft Visual Studio\2019\Community\VC\Auxiliary\Build\vcvarsall.bat" %VS_ARCH% ^
   && qmake ^
   && jom ^
   && bin\phantomjs --version ^
